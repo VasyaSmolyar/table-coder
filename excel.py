29b4a67c31e36548319cc8db32be508912fb0626
VasyaSmolyar <smolyar4@gmail.com>
2020-07-24 00:58:37 +0300

    Работа с индексами

--- excel.py
+++ excel.py
@@ -4,15 +4,30 @@ def get_index(line):
     line = str(line)
     return int(line.split('.')[0])
 
+def mnemonic(a):
+    if len(a) == 1:
+        res = ord(a) - ord('A') + 1
+    else:
+        res = (ord(a[0]) - ord('A') + 1) * 26 + ord(a[1]) - ord('A') + 1
+    return res
+
+def amnemonic(res):
+    if res > 26:
+        code = chr(res // 26 + ord('A') - 1)
+        code += chr(res % 26 + ord('A') - 1)
+    else:
+        code = chr(res + ord('A') - 1)
+    return code
+
 def get_len(start, stop):
-    return ord(stop) - ord(start)
+    return mnemonic(stop.upper()) - mnemonic(start.upper()) + 1
 
 def get_name(start, index):
-    return chr(ord(start) + index)
+    return amnemonic(mnemonic(start) + index)
 
 def by_index(filename, listname, x_start, x_stop, y_start, y_stop, fields):
     length = get_len(x_start, x_stop)
-    res = [['#' for i in range(length-3)] for i in range(y_stop - y_start + 2)]
+    res = [['\t' for i in range(length-3)] for i in range(y_stop - y_start + 2)]
     wb = load_workbook(filename=filename)
     ws = wb[listname]
 
@@ -28,4 +43,4 @@ def by_index(filename, listname, x_start, x_stop, y_start, y_stop, fields):
                 break
     return res
 
-#print(by_index('test.xlsx', 'Лист1', 'B', 'G', 4, 16, [str(x) + '.' + 'Stonks' for x in range(1, 15)]))
+#print(by_index('test.xlsx', 'Лист1', 'B', 'AB', 4, 16, [str(x) + '.' + 'Stonks' for x in range(1, 15)]))
