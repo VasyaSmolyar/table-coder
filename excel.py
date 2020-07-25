005a06c262decaa4f52400e8e1b2fca908e0d12c
VasyaSmolyar <smolyar4@gmail.com>
2020-07-25 03:45:10 +0300

    Добавлен нечёткий поиск по строкам

--- excel.py
+++ excel.py
@@ -1,4 +1,5 @@
 from openpyxl import load_workbook
+from fuzzywuzzy import fuzz
 
 def get_index(line):
     line = str(line)
@@ -29,6 +30,19 @@ def get_len(start, stop):
 def get_name(start, index):
     return amnemonic(mnemonic(start) + index)
 
+def unzip(s):
+    mas = s.split('.')
+    ind = 0 if len(mas) == 1 else 1
+    f = mas[ind].lower() 
+    mas2 = f.split(',')
+    return mas2[0].strip()
+
+def compare(src, dest, scope=60):
+    a, b = (unzip(str(src).strip()), unzip(str(dest).strip()))
+    #if fuzz.token_set_ratio(a, b) >= scope:
+        #print(a, '###' ,b)
+    return fuzz.token_set_ratio(a, b) >= scope
+
 def by_index(filename, listname, x_start, x_stop, y_start, y_stop, fields):
     length = get_len(x_start, x_stop)
     res = [['\t' for i in range(length-3)] for i in range(y_stop - y_start + 2)]
@@ -37,13 +51,19 @@ def by_index(filename, listname, x_start, x_stop, y_start, y_stop, fields):
 
     for i in range(y_start, y_stop + 1):
         for j in range(len(fields)):
-            ind = get_index(ws[x_start + str(i)].value)
-            if ind == get_index(fields[j]):
+            #ind = get_index(ws[x_start + str(i)].value)
+            #if ind == get_index(fields[j]):
+            #ws[x_start + str(i)].value
+            if compare(ws[get_name(x_start, 1) + str(i)].value, fields[j]):
+                ind = get_index(fields[j])
+                print(ind)
+                if res[ind-1][0] != '\t':
+                    continue
                 for k in range(3, length):
                     if ws[get_name(x_start, k) + str(i)].value == None:
-                        res[get_index(ws[x_start + str(i)].value) - 1][k-3] = ''
+                        res[ind-1][k-3] = ''
                     else:
-                        res[get_index(ws[x_start + str(i)].value) - 1][k-3] = ws[get_name(x_start, k) + str(i)].value
+                        res[ind-1][k-3] = ws[get_name(x_start, k) + str(i)].value
                 break
     return res
 
