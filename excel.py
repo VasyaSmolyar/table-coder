f114973f0a6087abcc50538d0d338844251c02c7
VasyaSmolyar <smolyar4@gmail.com>
2020-07-27 00:39:23 +0300

    Рефакторинг

--- excel.py
+++ excel.py
@@ -31,9 +31,9 @@ def get_name(start, index):
     return amnemonic(mnemonic(start) + index)
 
 def unzip(s):
-    mas = s.split('.')
-    ind = 0 if len(mas) == 1 else 1
-    f = mas[ind].lower() 
+    #mas = s.split('.')
+    #ind = 0 if len(mas) == 1 else 1
+    f = s.lower() 
     mas2 = f.split(',')
     return mas2[0].strip()
 
@@ -46,9 +46,9 @@ def compare(src, dest, scope=40):
     r, ind = best_match(unzip(str(src).strip()), dest)
     return (r >= scope, ind)
 
-def by_index(filename, listname, x_start, x_stop, y_start, y_stop, fields):
+def by_name(filename, listname, x_start, x_stop, y_start, y_stop, fields):
     length = get_len(x_start, x_stop)
-    res = [['\t' for i in range(length-3)] for i in range(y_stop - y_start + 2)]
+    res = [['\t' for i in range(length-3)] for i in range(len(fields))]
     wb = load_workbook(filename=filename)
     ws = wb[listname]
 
@@ -64,4 +64,20 @@ def by_index(filename, listname, x_start, x_stop, y_start, y_stop, fields):
                     res[ind][k-3] = ws[get_name(x_start, k) + str(i)].value
     return res
 
-#print(by_index('test.xlsx', 'Лист1', 'B', 'AB', 4, 16, [str(x) + '.' + 'Stonks' for x in range(1, 15)]))
+def by_index(filename, listname, x_start, x_stop, y_start, y_stop, fields):
+    length = get_len(x_start, x_stop)
+    res = [['\t' for i in range(length-1)] for i in range(len(fields))]
+    wb = load_workbook(filename=filename)
+    ws = wb[listname]
+
+    for i in range(y_start, y_stop + 1):
+        for j in range(len(fields)):
+            if get_index(ws[get_name(x_start, 0) + str(i)]) == get_index(fields[j]):
+                for k in range(1, length):
+                    if ws[get_name(x_start, k) + str(i)].value == None:
+                        res[ind][k-1] = ''
+                    else:
+                        res[ind][k-1] = ws[get_name(x_start, k) + str(i)].value
+    return res
+
+#print(by_name('test.xlsx', 'Лист1', 'B', 'AB', 4, 16, [str(x) + '.' + 'Stonks' for x in range(1, 15)]))
--- web.py
+++ web.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template, request
-from excel import by_index
+from excel import by_name, by_index
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    args = ['' for i in range(6)]
+    if request.method == 'POST':
+        if 'sheet' in request.files:
+            file = request.files['sheet']
+            fname = 'uploaded_file.xlsx'
+            file.save(fname)
+            fields = request.form.get('fields', '').split('\n')
+            res = by_name(fname, request.form.get('list'), request.form.get('x_start'), 
+                request.form.get('x_stop'), int(request.form.get('y_start')), int(request.form.get('y_stop')), 
+                fields
+            )
+            args = [request.form.get('list'), request.form.get('x_start'), 
+                request.form.get('x_stop'), request.form.get('y_start'), request.form.get('y_stop'), request.form.get('fields', '')]
+            return render_template('root.html', res=res, args=args, name="По заголовку")
+    return render_template('root.html', args=args, name="По заголовку")
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
     args = ['' for i in range(6)]
     if request.method == 'POST':
         if 'sheet' in request.files:
@@ -18,8 +36,8 @@ def main():
             )
             args = [request.form.get('list'), request.form.get('x_start'), 
                 request.form.get('x_stop'), request.form.get('y_start'), request.form.get('y_stop'), request.form.get('fields', '')]
-            return render_template('root.html', res=res, args=args)
-    return render_template('root.html', args=args)
+            return render_template('root.html', res=res, args=args, name="По индексу")
+    return render_template('root.html', args=args, name="По индексу")
 
 if __name__ == '__main__':
     app.run(debug=True)
